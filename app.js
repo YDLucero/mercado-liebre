0645c0c3cf12739615ef464d9461620d6bbc1f93
YDLucero <82624598+YDLucero@users.noreply.github.com>
2023-11-07 11:10:18 -0300

    ML-adaptativo

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 let port = 3000;
 let path = require('path');
 
-/* Routing */
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html')));
-
 /* Middleware */
 app.use(express.static('public'));
 
+/* Routing */
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html')));
+
 /* Server */
 app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}\n http://localhost:${port}`));
-
