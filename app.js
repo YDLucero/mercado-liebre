3a7c574a0500de6ffe7ff33d6c307d5058770383
YDLucero <yohanna.lucero@gmail.com>
2023-11-07 16:38:59 -0300

    ajustes menores

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(express.static('public'));
 
 /* Routing */
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/views/home.html')));
+app.get('/login', (req, res) => res.sendFile(path.join(__dirname, '/views/login.html')));
+app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '/views/register.html')));
 
 /* Server */
 app.listen(port, () => console.log(`Servidor levantado en el puerto ${port}\n http://localhost:${port}`));
